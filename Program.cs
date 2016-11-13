db3dece1df96f720a5e12fadc807a4c9888bef93
vbrh-immalle <hans.vanbroeckhoven@immalle.be>
2016-11-13 14:25:16 +0100

    Console

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
