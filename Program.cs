5a832d1be700fed8519f2aa76cc22481517c571a
vbrh-immalle <hans.vanbroeckhoven@immalle.be>
2016-11-20 11:25:12 +0100

    Makedir and Removedir both working

--- Program.cs
+++ Program.cs
@@ -1,58 +1,124 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace ConsoleApplication
 {
-    public class MakeDir {
-        public enum Status {
-                    NoNameGiven,
-                    DirMade,
-                    DirExists
-                }
+    public class MakeDir
+    {
+        private enum Result
+        {
+            NoNameGiven,
+            DirMade,
+            DirExists
+        }
 
-        static Dictionary<Status, string> melding = new Dictionary<Status, string>() {
-                { Status.NoNameGiven, "Kan directory niet maken: naam niet opgegeven." },
-                { Status.DirMade, "Directory [{0}] gemaakt." },
-                { Status.DirExists, "Directory [{0}] bestaat al." }
+        private static Dictionary<Result, string> melding = new Dictionary<Result, string>() {
+                { Result.NoNameGiven, "Kan directory niet maken: naam niet opgegeven." },
+                { Result.DirMade, "Directory [{0}] gemaakt." },
+                { Result.DirExists, "Directory [{0}] bestaat al." }
             };
 
-        static Dictionary<Status, ConsoleColor> kleur = new Dictionary<Status, ConsoleColor>() {
-                { Status.NoNameGiven, ConsoleColor.Red },
-                { Status.DirMade, ConsoleColor.Green },
-                { Status.DirExists, ConsoleColor.Yellow }
+        private static Dictionary<Result, ConsoleColor> kleur = new Dictionary<Result, ConsoleColor>() {
+                { Result.NoNameGiven, ConsoleColor.Red },
+                { Result.DirMade, ConsoleColor.Green },
+                { Result.DirExists, ConsoleColor.Yellow }
             };
 
-        public static void HandleState(Status status, string dirnaam = "") {          
+        private static void DisplayState(Result status, string dirnaam = "")
+        {
             Console.ForegroundColor = kleur[status];
             Console.WriteLine(melding[status], dirnaam);
             Console.ResetColor();
         }
+
+        public static void Run(string[] args)
+        {
+            if (args.Length <= 0)
+            {
+                DisplayState(Result.NoNameGiven);
+            }
+            else
+            {
+                string naam = args[0];
+                if (Directory.Exists(naam))
+                {
+                    DisplayState(Result.DirExists, naam);
+                }
+                else
+                {
+                    Directory.CreateDirectory(naam);
+                    DisplayState(Result.DirMade, naam);
+                }
+            }
+        }
+
     }
 
-    public class RemoveDir {
-        public enum Status {
-                    NoNameGiven,
-                    DirRemoved,
-                    DirDoesntExist
+
+    public class RemoveDir
+    {
+        public enum Result
+        {
+            NoNameGiven,
+            DirRemoved,
+            DirDoesntExist,
+            DirNotEmpty
+        }
+
+        static Dictionary<Result, string> melding = new Dictionary<Result, string>() {
+                { Result.NoNameGiven, "Kan directory niet verwijderen: naam niet opgegeven." },
+                { Result.DirRemoved, "Directory [{0}] verwijderd." },
+                { Result.DirDoesntExist, "Kan directory niet verwijderen: directory [{0}] bestaat niet." },
+                { Result.DirNotEmpty, "Directory [{0}] niet leeg, bevat [{1}] items." }
+            };
+
+        static Dictionary<Result, ConsoleColor> kleur = new Dictionary<Result, ConsoleColor>() {
+                { Result.NoNameGiven, ConsoleColor.Red },
+                { Result.DirRemoved, ConsoleColor.Green },
+                { Result.DirDoesntExist, ConsoleColor.Yellow },
+                { Result.DirNotEmpty, ConsoleColor.Yellow }
+            };
+
+        private static void DisplayState(Result status, string dirnaam = "", int aantalDirItems = 0)
+        {
+            Console.ForegroundColor = kleur[status];
+            Console.WriteLine(melding[status], dirnaam, aantalDirItems);
+            Console.ResetColor();
+        }
+
+        public static void Run(string[] args)
+        {
+            if (args.Length <= 0)
+            {
+                DisplayState(Result.NoNameGiven);
+            }
+            else
+            {
+                string naam = args[0];
+                if (!Directory.Exists(naam))
+                {
+                    DisplayState(Result.DirNotEmpty, naam);
                 }
-
-        static Dictionary<Status, string> melding = new Dictionary<Status, string>() {
-                { Status.NoNameGiven, "Kan directory niet verwijderen: naam niet opgegeven." },
-                { Status.DirRemoved, "Directory [{0}] verwijderd." },
-                { Status.DirDoesntExist, "Kan directory niet verwijderen: directory [{0}] bestaat niet." }
-            };
-
-        static Dictionary<Status, ConsoleColor> kleur = new Dictionary<Status, ConsoleColor>() {
-                { Status.NoNameGiven, ConsoleColor.Red },
-                { Status.DirRemoved, ConsoleColor.Green },
-                { Status.DirDoesntExist, ConsoleColor.Yellow }
-            };
+                else {
+                    int aantalDirItems = Directory.GetFileSystemEntries(naam).Length;
+                    if( aantalDirItems > 0) {
+                        DisplayState(Result.DirNotEmpty, naam, aantalDirItems);
+                    } else {
+                        Directory.CreateDirectory(naam);
+                        Directory.Delete(naam);
+                        DisplayState(Result.DirRemoved, naam);
+                    }
+                }
+            }
+        }
     }
 
     public class Program
     {
-        enum Status {
+        enum Status
+        {
             InvalidCommand,
             MakeDirCommand,
             RemoveDirCommand
@@ -63,17 +129,26 @@ namespace ConsoleApplication
             Console.Clear();
             Console.WriteLine("Hello World!");
 
-            if(args.Length <= 1) {
-                MakeDir.HandleState(MakeDir.Status.NoNameGiven);
-            } else {
-                string naam = args[0];
-                if(Directory.Exists(naam)) {
-                    MakeDir.HandleState(MakeDir.Status.DirExists, naam);
-                } else {
-                    Directory.CreateDirectory(naam);
-                    MakeDir.HandleState(MakeDir.Status.DirMade, naam);
-                }
+            if(args.Length <= 0) {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Not a command!");
+                System.Environment.Exit(1);
             }
+        
+            switch (args[0])
+            {
+                case "makedir":
+                    MakeDir.Run(args.Skip(1).ToArray());
+                    break;
+                case "removedir":
+                    RemoveDir.Run(args.Skip(1).ToArray());
+                    break;
+                default:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Unknown command [{0}].", args[0]);
+                    break;
+            }
+
         }
     }
 }
